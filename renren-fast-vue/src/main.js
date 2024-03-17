1e25f1c2567edca346132a236a5cb7f1cc802a10
vcciccv <chaochen497@gmail.com>
2024-03-17 08:48:14 +0800

    品牌

--- renren-fast-vue/src/main.js
+++ renren-fast-vue/src/main.js
@@ -1,36 +1,46 @@
+/*
+ * @Author: vcciccv chaochen497@gmail.com
+ * @Date: 2023-12-29 10:08:02
+ * @LastEditors: vcciccv chaochen497@gmail.com
+ * @LastEditTime: 2024-03-16 20:49:27
+ * @FilePath: \renren-fast-vue\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import App from '@/App'
-import router from '@/router'                 // api: https://github.com/vuejs/vue-router
-import store from '@/store'                   // api: https://github.com/vuejs/vuex
-import VueCookie from 'vue-cookie'            // api: https://github.com/alfhen/vue-cookie
-import '@/element-ui'                         // api: https://github.com/ElemeFE/element
-import '@/icons'                              // api: http://www.iconfont.cn/
+import router from '@/router' // api: https://github.com/vuejs/vue-router
+import store from '@/store' // api: https://github.com/vuejs/vuex
+import VueCookie from 'vue-cookie' // api: https://github.com/alfhen/vue-cookie
+import '@/element-ui' // api: https://github.com/ElemeFE/element
+import '@/icons' // api: http://www.iconfont.cn/
 import '@/element-ui-theme'
 import '@/assets/scss/index.scss'
 import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
 import { isAuth } from '@/utils'
 import cloneDeep from 'lodash/cloneDeep'
+import PubSub from 'pubsub-js'
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
 
 // 非生产环境, 适配mockjs模拟数据                 // api: https://github.com/nuysoft/Mock
 if (process.env.NODE_ENV !== 'production') {
-  require('@/mock')
+    require('@/mock')
 }
 
 // 挂载全局
 Vue.prototype.$http = httpRequest // ajax请求方法
-Vue.prototype.isAuth = isAuth     // 权限方法
+Vue.prototype.isAuth = isAuth // 权限方法
+Vue.prototype.PubSub = PubSub
 
 // 保存整站vuex本地储存初始状态
 window.SITE_CONFIG['storeState'] = cloneDeep(store.state)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  store,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    router,
+    store,
+    template: '<App/>',
+    components: { App }
 })
